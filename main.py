f8a3c2cc67a21fb63140a5bada3485da61dfcdba
Elliot Glaze <elliotglaze98@gmail.com>
2020-06-22 08:28:59 +0100

    Error handling Complete

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+app.secret_key = 'sdfh9e34gkggdf'
 
 
 @app.route('/', methods=['GET'])
@@ -14,13 +15,30 @@ def home():
 def form_input():
     artist_name = ''
     song_name = ''
-
     for key, value in request.form.items():
         if key == 'artist_name':
             artist_name += value
         elif key == 'song_name':
             song_name += value
 
-    return artist_name + " " + song_name
+    artist_plus_song = artist_name + " " + song_name
+    query = artist_plus_song.replace(" ", "+")
+
+    # GETS MATCH ON YOUTUBE
+    url = 'https://www.youtube.com/results?search_query=' + query
+    source = requests.get(url, timeout=15)
+    plain_text = source.text
+    soup = BeautifulSoup(plain_text, "html.parser")
+
+    try:
+        # FETCHES THE URL
+        songs = soup.findAll('div', {'class': 'yt-lockup-video'})
+        song = songs[0].contents[0].contents[0].contents[0]
+        link = song['href']
+        return "https://www.youtube.com" + link
+    except Exception as e:
+        print("Can't find that, check your network or try a new song", e)
+        flash("Can't find that, check your network or try a new song")
+        return redirect(url_for('home'))
 
 
