2c6c89586cd6c39e40a4bb351b624d673d8068d1
Elliot Glaze <elliotglaze98@gmail.com>
2020-06-22 07:48:19 +0100

    Create form and get user input

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+
+@app.route('/post_field', methods=['GET', 'POST'])
+def form_input():
+    artist_name = ''
+    song_name = ''
+
+    for key, value in request.form.items():
+        if key == 'artist_name':
+            artist_name += value
+        elif key == 'song_name':
+            song_name += value
+
+    return artist_name + " " + song_name
 
 
